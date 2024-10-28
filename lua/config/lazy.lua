0f7855de1e33e2bd30006608d46216f2c3794549
Semyon Dunaev <dunaevsemyon@gmail.com>
2024-10-28 14:56:39 +0200

    bla-bla

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
   },
---
+++ lua/config/terminal.lua
@@ -0,0 +1,5 @@
+require("toggleterm").setup({
+  open_mapping = [[<c-\>]],
+  size = 12,
+  dir = "git_dir",
+})
